fdfa130cf96649ff33beac6690964f7ad00c8e3a
xconstruct <xconstruct@gmail.com>
2009-10-21 16:40:36 +0200

    promoting bad habits

--- LibCargEvents-1.0.lua
+++ LibCargEvents-1.0.lua
@@ -6,32 +6,41 @@ License: GPL 2
 Description: Library to handle events
 ]]
 
-local lib = LibStub:NewLibrary("LibCargEvents-1.0", 2)
+local lib = LibStub:NewLibrary("LibCargEvents-1.0", 3)
 if(not lib) then return end
 
+local lazyCounter = 0
 local events = {}
 local metahandlers = {}
 local frame = CreateFrame("Frame")
 function lib.RegisterEvent(self, event, func)
+	if(not func and type(event) == "table") then
+		lazyCounter = lazyCounter + 1
+		func, event, self = event, self, lazyCounter
+	elseif(self == lib) then
+		lazyCounter = lazyCounter + 1
+		self = lazyCounter
+	end
 	events[event] = events[event] or {}
 	local thisEvent = events[event]
 	thisEvent[self] = func
-	if(thisEvent.count) then
-		thisEvent.count = thisEvent.count+1
+	if(thisEvent.__count) then
+		thisEvent.__count = thisEvent.__count+1
 	else
-		thisEvent.count = 1
+		thisEvent.__count = 1
 		frame:RegisterEvent(event)
 	end
+	return self
 end
 
 function lib.UnregisterEvent(self, event)
 	if(not lib.IsEventRegistered(self, event)) then return end
 	local thisEvent = events[event]
 	thisEvent[self] = nil
-	thisEvent.count = thisEvent.count-1
-	if(thisEvent.count == 0) then
+	thisEvent.__count = thisEvent.__count-1
+	if(thisEvent.__count == 0) then
 		frame:UnregisterEvent(event)
-		thisEvent.count = nil
+		thisEvent.__count = nil
 	end
 end
 
@@ -51,8 +60,10 @@ end
 gEvents = events
 frame:SetScript("OnEvent", function(self, event, ...)
 	for frame, func in pairs(events[event]) do
-		if(frame ~= "count" and (not metahandlers[self] or metahandlers[self](self, event, ...))) then
+		if(frame ~= "__count" and (not metahandlers[self] or metahandlers[self](self, event, ...))) then
 			func(frame, event, ...)
 		end
 	end
 end)
+
+setmetatable(lib, {__call = lib.RegisterEvent})
